8e093700430377f829d18b0161ace48485e524ab
cesarve-dev <cesar.radiate@gmail.com>
2026-05-12 01:17:15 -0700

    implementation for assignment 5b.

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const crypto = require("crypto");
 const util = require("util");
 const scrypt = util.promisify(crypto.scrypt);
 const { userSchema } = require("../validation/userSchema");
+const pool = require("../db/pg-pool");
 
 async function hashPassword(password) {
   const salt = crypto.randomBytes(16).toString("hex");
@@ -17,35 +18,55 @@ async function comparePassword(inputPassword, storedHash) {
   return crypto.timingSafeEqual(keyBuffer, derivedKey);
 }
 
-const register = async (req, res) => {
+const register = async (req, res, next) => {
   if (!req.body) req.body = {};
-  const { error, value } = userSchema.validate(req.body);
+  const { error, value } = userSchema.validate(req.body, { abortEarly: false });
   if (error) {
-    return res.status(StatusCodes.BAD_REQUEST).json({ message: error.message });
+    return res
+      .status(400)
+      .json({ message: "Validation failed", details: error.details });
   }
-  const { name, email, password } = value;
-  const hashedPass = await hashPassword(password);
-  const newUser = { name, email, hashedPass };
-  global.users.push(newUser);
-  global.user_id = newUser;
 
-  res.status(StatusCodes.CREATED).json({ name, email });
+  let user = null;
+  value.hashed_password = await hashPassword(value.password);
+
+  try {
+    user = await pool.query(
+      `INSERT INTO users (email, name, hashed_password) 
+      VALUES ($1, $2, $3) RETURNING id, email, name`,
+      [value.email, value.name, value.hashed_password],
+    ); // note that you use a parameterized query
+  } catch (e) {
+    // the email might already be registered
+    if (e.code === "23505") {
+      return res.status(400).json({ message: e.message });
+    }
+    return next(e); // all other errors get passed to the error handler
+  }
+  global.user_id = user.rows[0].id;
+
+  res
+    .status(StatusCodes.CREATED)
+    .json({ name: user.rows[0].name, email: user.rows[0].email });
 };
 
-const logon = async (req, res) => {
+const logon = async (req, res, next) => {
   const { email, password } = req.body;
-  const existingUser = global.users.find((user) => user.email === email);
+  const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
+  const existingUser = result.rows[0];
 
   if (
     !existingUser ||
-    !(await comparePassword(password, existingUser.hashedPass))
+    !(await comparePassword(password, existingUser.hashed_password))
   ) {
     return res
       .status(StatusCodes.UNAUTHORIZED)
       .json({ message: "Authentication Failed" });
   }
 
-  global.user_id = existingUser;
+  global.user_id = result.rows[0].id;
   res.status(StatusCodes.OK).json({
     message: "User verified",
     name: existingUser.name,
--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ const js = require("@eslint/js");
 const globals = require("globals");
 
 module.exports = defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], rules: { "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }] } },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs", globals: { ...globals.node} } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } }
 ]);
